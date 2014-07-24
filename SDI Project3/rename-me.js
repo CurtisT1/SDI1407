8105b72de024b2cd70fb701fdd76ab864e04df80
CurtisT1 <curtist2fullsail.edu>
2014-07-24 04:47:51 -0500

    First

--- SDI Project3/rename-me.js
+++ SDI Project3/rename-me.js
@@ -41,27 +41,6 @@ alert("Welcome Back");
 };
 
 
-// Function
-    
-    var  chooseWeapon = function (warlockName, weapon1) {
-		console.log("choose " + weapon1 + " warlock" + warlockName);
-	var useWeapon = function(warlockName) {
-	var weapon1 = "wand";
-		console.log("choose " + weapon1 + warlockName);
-	};	 
-	useWeapon("warlock");
-	useWeapon("witch1");
-	console.log(warlockName + " choose " + weapon1);
-}; 
-
-console.log("The warlock arrives at the witch hideout");
-console.log("",witch1,"conjures the biggest ice ball at him. ",warlock," conjure a wall of fire and stops it, and had to quickly choose" ,weapon1," or " ,weapon2,'.');
-console.log("",weapon1," is faster and gives " ,warlock, " more movement. ",weapon2," is heavy and drains half of his magic energy.");
-console.log("",warlock, " quickly conjure a force over his body and went into a powerful trance to recharge.");
-var recharge = {
-	
-};
-
 
 
 
