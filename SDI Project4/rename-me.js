93ecf16165bc925c6e66717ae39c4b7da6fd13e8
CurtisT1 <curtist2fullsail.edu>
2014-07-31 05:54:55 -0500

    First Problem

--- SDI Project4/rename-me.js
+++ SDI Project4/rename-me.js
@@ -1,1 +1,42 @@
 alert("JavaScript works!");
+
+/*Curtis Tate
+SDI 1407
+Assignment 4 
+July 31 2014*/
+
+
+// Global Variables 
+
+var phoneReturn
+var urlReturn
+
+
+// string Function
+
+
+var phoneNumber = function(number) {
+	var firstDash = number.charAt (3);
+	var secondDash = number.charAt (7);
+	var firstNumbers = number.substring (0,3);
+	var secondNumbers = number.substring (4,7);
+	var thirdNumbers = number.substring (8,12);
+	
+	
+	if (firstDash != "-") {
+		return false;
+	} else if (secondDash != "-") {
+		return false;
+	} else if (firstNumbers >= "0" && firstNumbers <= "9") {
+		return true;
+	} else if (secondNumbers >= "0" && secondNumbers <= "9") {
+		return true;
+	} else if (thirdNumber >= "0" && secondNumbers <= "9") {
+		return true;
+	} else if (number != 12) {
+		return false;
+	} else return false;
+	
+		return phoneReturn};
+	
+	
